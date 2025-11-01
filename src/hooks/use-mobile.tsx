14e19e9aa50409c9322d7f2dcc60d3a47e0ce781
Lakshmi Narayana Gupta Boddu <lakshminarayanaguptaboddu@gmail.com>
2025-11-01 10:08:34 +0000

    remove the mobile 1st rule

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -4,17 +4,22 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(false)
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
+    // We set it to false initially and don't change it, effectively disabling mobile view.
+    setIsMobile(false);
+    
+    // The event listener is kept in case we want to re-enable it later,
+    // but it won't affect the initial `false` state.
     mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
-  return !!isMobile
+  return false;
 }
