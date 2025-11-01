c1184ea47834a899daadb10ed9d43860b6be7e01
Lakshmi Narayana Gupta Boddu <lakshminarayanaguptaboddu@gmail.com>
2025-11-01 14:59:32 +0000

    change another logo

--- src/app/components/shared/logo.tsx
+++ src/app/components/shared/logo.tsx
@@ -13,6 +13,7 @@ export function Logo({ className }: { className?: string }) {
       >
         <path
           d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+          fill="currentColor"
           stroke="currentColor"
           strokeWidth="2"
           strokeLinecap="round"
@@ -20,14 +21,14 @@ export function Logo({ className }: { className?: string }) {
         />
         <path
           d="M16.24 7.76001L7.76001 16.24"
-          stroke="currentColor"
+          stroke="white"
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
         <path
           d="M16.24 16.24L7.76001 7.76001"
-          stroke="currentColor"
+          stroke="white"
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
