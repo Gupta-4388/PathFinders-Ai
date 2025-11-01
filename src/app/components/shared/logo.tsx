7bbfb19a44b2ff49ec441658463f1942796acd20
Lakshmi Narayana Gupta Boddu <lakshminarayanaguptaboddu@gmail.com>
2025-11-01 15:30:09 +0000

    change the logo into path logo

--- src/app/components/shared/logo.tsx
+++ src/app/components/shared/logo.tsx
@@ -10,28 +10,21 @@ export function Logo({ className }: { className?: string }) {
         xmlns="http://www.w3.org/2000/svg"
       >
         <g>
-          <circle cx="50" cy="50" r="45" fill="none" stroke="#f97316" strokeWidth="5" />
-          <circle cx="50" cy="50" r="38" fill="none" stroke="#2563eb" strokeWidth="5" />
-          <path 
-            d="M 50,95 
-               C 50,95 20,75 30,50 
-               S 45,20 50,10 
-               s 20,40 20,40 
-               c 0,0 -10,25 -20,45 z" 
-            fill="#333" 
-            transform="scale(1, 0.9) translate(0, 5)"
+          <circle cx="50" cy="50" r="45" fill="#2563eb" />
+          <path
+            d="M 30 70 Q 50 50, 70 30"
+            stroke="#f97316"
+            strokeWidth="10"
+            strokeLinecap="round"
+            fill="none"
           />
-          <path 
-            d="M 50,85 
-               C 50,85 40,70 45,50 
-               S 48,30 50,20 
-               s 5,20 5,30 
-               c 0,0 -5,15 -5,35 z" 
-            fill="none" 
-            stroke="white" 
-            strokeWidth="1.5" 
-            strokeDasharray="5, 3" 
-            transform="scale(1, 0.9) translate(0, 5)"
+           <path
+            d="M 30 70 Q 50 50, 70 30"
+            stroke="white"
+            strokeWidth="3"
+            strokeDasharray="5 5"
+            strokeLinecap="round"
+            fill="none"
           />
         </g>
       </svg>
