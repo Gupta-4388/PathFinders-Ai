670c2abf9785c55b3bbc9c43e0c3a33590b31cc0
Lakshmi Narayana Gupta Boddu <lakshminarayanaguptaboddu@gmail.com>
2025-11-01 09:39:28 +0000

    reframe the windows panel configurations

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,7 +1,7 @@
 "use client";
 import * as React from "react"
 
-const MOBILE_BREAKPOINT = 0
+const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
