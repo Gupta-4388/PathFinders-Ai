9dfefb98aa187d885f26e5c74ebeb66314444b54
Lakshmi Narayana Gupta Boddu <lakshminarayanaguptaboddu@gmail.com>
2025-11-04 16:47:09 +0000

    it is showing an error of can't fetch the first interview question fix a

--- app/src/app/(app)/interview/page.tsx
+++ app/src/app/(app)/interview/page.tsx
@@ -1,17 +1,12 @@
 
 'use client';
+
 import { InterviewSimulator } from '@/app/components/interview/interview-simulator';
 
-function InterviewPageContent() {
+export default function InterviewPage() {
   return (
     <div className="max-w-4xl mx-auto h-[calc(100vh-10rem)]">
       <InterviewSimulator />
     </div>
-  );
-}
-
-export default function InterviewPage() {
-  return (
-      <InterviewPageContent />
   );
 }
